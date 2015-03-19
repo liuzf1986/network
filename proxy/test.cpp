7fd7e0175f82fb834d9544e774b3864e71105fc9
liuzf1986 <liuzf1986@163.com>
2015-03-19 18:51:31 +0800

    modify proxy

--- proxy/test.cpp
+++ proxy/test.cpp
@@ -5,32 +5,20 @@
 
 #include "Proxy.hpp"
 #include "InetSock.hpp"
+#include "TcpProxy.hpp"
 
 #include "Dispatcher.hpp"
+#include "FieldLenNetPack.hpp"
 
 
 int main(int argc, char *argv[])
 {
-  Dispatcher<int> dispatcher;
+  shared_ptr<LooperPool<MultiplexLooper> > loopers(new LooperPool<MultiplexLooper>(5));
+  TcpProxy<FieldLenNetpack<GenericLenFieldHeader> > proxy(loopers, static_cast<uint16_t>(3002), static_cast<uint16_t>(8550));
 
+  sleep(10);
   
   return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
